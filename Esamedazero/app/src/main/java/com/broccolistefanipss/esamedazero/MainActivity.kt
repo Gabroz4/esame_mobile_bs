8ffbb836656fdc179fc1ccdfd5c1b51cf4e7e683
Gabroz4 <gabriele.broccoli@gmail.com>
2024-02-16 17:51:25 +0100

    Inizializzazione da zero

---
+++ Esamedazero/app/src/main/java/com/broccolistefanipss/esamedazero/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.broccolistefanipss.esamedazero
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val nextButton = findViewById<Button>(R.id.nextButton)
+
+        val intent = Intent(this, BotMenuActivity::class.java)
+
+        nextButton.setOnClickListener {
+            startActivity(intent)
+        }
+
+    }
+}
