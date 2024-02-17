38b412aee85df25d20dbeec47624b96ac683c6e8
Gabroz4 <gabriele.broccoli@gmail.com>
2024-02-17 15:18:01 +0100

    Ordine cartelle

--- Esempione/app/src/main/java/it/unibo/lam/shop/data/product/Category.kt
+++
@@ -1,7 +0,0 @@
-package it.unibo.lam.shop.data.product
-
-data class Category(
-    val id: Int,
-    val name: String,
-    val image: String
-)
--- Esempione/app/src/main/java/it/unibo/lam/shop/data/product/ProductRepository.kt
+++
@@ -1,6 +0,0 @@
-package it.unibo.lam.shop.data.product
-
-abstract class ProductRepository {
-    abstract fun getProducts(): List<Product>?;
-    abstract fun getProduct(productID: Int): Product?;
-}
