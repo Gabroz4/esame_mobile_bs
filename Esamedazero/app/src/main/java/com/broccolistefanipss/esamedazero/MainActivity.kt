a51c897588155179fc6055d78c0a5a30a0f8e5f0
Gabroz4 <gabriele.broccoli@gmail.com>
2024-02-17 16:30:36 +0100

    Loading screen

--- Esamedazero/app/src/main/java/com/broccolistefanipss/esamedazero/MainActivity.kt
+++ Esamedazero/app/src/main/java/com/broccolistefanipss/esamedazero/MainActivity.kt
@@ -3,21 +3,26 @@ package com.broccolistefanipss.esamedazero
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.broccolistefanipss.esamedazero.activity.BotMenuActivity
+import android.os.Handler
+import com.broccolistefanipss.esamedazero.activity.WelcomeActivity
 
 class MainActivity : AppCompatActivity() {
+    private var delayHandler : Handler? = null
+    private val loading_delay: Long = 2000 //2 sec
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val nextButton = findViewById<Button>(R.id.nextButton)
+        delayHandler?.postDelayed(mRunnable, loading_delay)
 
-        val intent = Intent(this, BotMenuActivity::class.java)
-
-        nextButton.setOnClickListener {
-            startActivity(intent)
-        }
-
+    }
+    private val mRunnable : Runnable = Runnable {
+        val intent = Intent(this, WelcomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        delayHandler?.removeCallbacks(mRunnable)
     }
 }
---
+++ Esamedazero/app/src/main/java/com/broccolistefanipss/esamedazero/activity/WelcomeActivity.kt
@@ -0,0 +1,23 @@
+package com.broccolistefanipss.esamedazero.activity
+
+import android.content.Intent
+import android.widget.Button
+import com.broccolistefanipss.esamedazero.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        val nextButton = findViewById<Button>(R.id.nextButton)
+
+        val intent = Intent(this, BotMenuActivity::class.java)
+
+        nextButton.setOnClickListener {
+            startActivity(intent)
+        }
+    }
+}
