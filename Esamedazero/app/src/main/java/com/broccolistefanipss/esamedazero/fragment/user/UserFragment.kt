9b9171c941ee1ae67bc21a81e270dd2d2c8335ff
Tommaso Stefani <tommasostefani3@gmail.com>
2024-03-30 17:16:55 +0100

    correzione logica stampa dati user

--- Esamedazero/app/src/main/java/com/broccolistefanipss/esamedazero/fragment/user/UserFragment.kt
+++ Esamedazero/app/src/main/java/com/broccolistefanipss/esamedazero/fragment/user/UserFragment.kt
@@ -1,6 +1,7 @@
 package com.broccolistefanipss.esamedazero.fragment.user
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,7 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.broccolistefanipss.esamedazero.databinding.FragmentUserBinding
+import com.broccolistefanipss.esamedazero.manager.SessionManager
 import com.broccolistefanipss.esamedazero.manager.SharedPrefs
+import sessionManager
 
 class UserFragment : Fragment() {
 
@@ -23,11 +26,16 @@ class UserFragment : Fragment() {
         val root: View = binding.root
 
         // Ottieni i dati dell'utente dalle SharedPreferences
-        val userName = SharedPrefs.getString(requireContext(), SharedPrefs.userName) ?: ""
-        val sex = SharedPrefs.getString(requireContext(), SharedPrefs.sesso) ?: ""
-        val age = SharedPrefs.getInt(requireContext(), SharedPrefs.eta) ?: 0
-        val height = SharedPrefs.getInt(requireContext(), SharedPrefs.altezza) ?: 0
-        val weight = SharedPrefs.getInt(requireContext(), SharedPrefs.peso) ?: 0
+        //val userName = SharedPrefs.getString(requireContext(), SharedPrefs.userName) ?: ""
+        val sessionManager = SessionManager(requireContext())
+        val userName = sessionManager.userName ?: ""
+
+        //val sex = SharedPrefs.getString(requireContext(), SharedPrefs.sesso) ?: ""
+        //val age = SharedPrefs.getInt(requireContext(), SharedPrefs.eta) ?: 0
+        //val height = SharedPrefs.getInt(requireContext(), SharedPrefs.altezza) ?: 0
+        //val weight = SharedPrefs.getInt(requireContext(), SharedPrefs.peso) ?: 0
+
+
 
         // Log dei dati recuperati
         Log.d("UserFragment", "userName: $userName")
@@ -44,12 +52,12 @@ class UserFragment : Fragment() {
         binding.WeightProfile.text = weight.toString()
 
         // Controllo di tutti i valori salvati nelle SharedPreferences
-        val sharedPreferences = requireContext().getSharedPreferences("UserData", Context.MODE_PRIVATE)
-        val allEntries: Map<String, *> = sharedPreferences.all
+        //val sharedPreferences = requireContext().getSharedPreferences("UserData", Context.MODE_PRIVATE)
+        //val allEntries: Map<String, *> = sharedPreferences.all
 
-        for ((key, value) in allEntries) {
-            Log.d("SharedPrefs", "$key: $value")
-        }
+        //for ((key, value) in allEntries) {
+        //    Log.d("SharedPrefs", "$key: $value")
+        //}
 
         return root
     }
