6ead618c31fc48c1abb6898888b3840793a783ce
Gabroz4 <gabriele.broccoli@gmail.com>
2024-09-23 12:15:06 +0200

    fix SessionManagerTest

--- SportsTracker/app/src/test/java/com/broccolistefanipss/sportstracker/SessionManagerTest.kt
+++ SportsTracker/app/src/test/java/com/broccolistefanipss/sportstracker/SessionManagerTest.kt
@@ -9,56 +9,61 @@ import org.mockito.Mockito.*
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 
-//class SessionManagerTest {
-//
-//    private lateinit var sessionManager: SessionManager
-//
-//    @Mock
-//    private lateinit var sharedPreferences: SharedPreferences
-//
-//    @Mock
-//    private lateinit var editor: SharedPreferences.Editor
-//
-//    @Before
-//    fun setUp() {
-//        MockitoAnnotations.openMocks(this)
-//        `when`(sharedPreferences.edit()).thenReturn(editor)
-//
-//        // simulazione del contesto
-//        val context = mock(Context::class.java)
-//        `when`(context.getSharedPreferences(SessionManager.PrefName, Context.MODE_PRIVATE)).thenReturn(sharedPreferences)
-//
-//        sessionManager = SessionManager(context)
-//    }
-//
-//    @Test
-//    fun testIsLoggedInWhenNotLoggedIn() {
-//        `when`(sharedPreferences.getBoolean(SessionManager.KeyIsLoggedIn, false)).thenReturn(false)
-//        assert(!sessionManager.isLoggedIn)
-//    }
-//
-//    @Test
-//    fun testSetLoginUpdatesLoginStatus() {
-//        sessionManager.setLogin(true)
-//        verify(editor).putBoolean(SessionManager.KeyIsLoggedIn, true)
-//        verify(editor).apply()
-//    }
-//
-//    @Test
-//    fun testUserNameGetterAndSetter() {
-//        val testUsername = "testUser"
-//        sessionManager.userName = testUsername
-//        verify(editor).putString(SessionManager.KeyUserName, testUsername)
-//        verify(editor).apply()
-//
-//        `when`(sharedPreferences.getString(SessionManager.KeyUserName, null)).thenReturn(testUsername)
-//        assert(sessionManager.userName == testUsername)
-//    }
-//
-//    @Test
-//    fun testUserNameReturnsNullWhenNotSet() {
-//        `when`(sharedPreferences.getString(SessionManager.KeyUserName, null)).thenReturn(null)
-//        assert(sessionManager.userName == null)
-//    }
-//}
+class SessionManagerTest {
+
+    private val prefName = "AndroidLoginPref"
+    private val keyUserName = "user_name"
+    private val keyIsLoggedIn = "IsLoggedIn"
+
+
+    private lateinit var sessionManager: SessionManager
+
+    @Mock
+    private lateinit var sharedPreferences: SharedPreferences
+
+    @Mock
+    private lateinit var editor: SharedPreferences.Editor
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        `when`(sharedPreferences.edit()).thenReturn(editor)
+
+        // simulazione del contesto
+        val context = mock(Context::class.java)
+        `when`(context.getSharedPreferences(prefName, Context.MODE_PRIVATE)).thenReturn(sharedPreferences)
+
+        sessionManager = SessionManager(context)
+    }
+
+    @Test
+    fun testIsLoggedInWhenNotLoggedIn() {
+        `when`(sharedPreferences.getBoolean(keyIsLoggedIn, false)).thenReturn(false)
+        assert(!sessionManager.isLoggedIn)
+    }
+
+    @Test
+    fun testSetLoginUpdatesLoginStatus() {
+        sessionManager.setLogin(true)
+        verify(editor).putBoolean(keyIsLoggedIn, true)
+        verify(editor).apply()
+    }
+
+    @Test
+    fun testUserNameGetterAndSetter() {
+        val testUsername = "testUser"
+        sessionManager.userName = testUsername
+        verify(editor).putString(keyUserName, testUsername)
+        verify(editor).apply()
+
+        `when`(sharedPreferences.getString(keyUserName, null)).thenReturn(testUsername)
+        assert(sessionManager.userName == testUsername)
+    }
+
+    @Test
+    fun testUserNameReturnsNullWhenNotSet() {
+        `when`(sharedPreferences.getString(keyUserName, null)).thenReturn(null)
+        assert(sessionManager.userName == null)
+    }
+}
 
